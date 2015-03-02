562e9a5cc696a160ddf733fd6446235934f4013f
vlad009 <vladkrilov9@gmail.com>
2015-03-02 22:54:54 +0100

    data file list routine finished

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-
+	if (argc < 2) {
+		cout <<""<<endl;
+		return 0;
+	}
 	cout << "Hello world2!" << endl;
+	return 0;
 }
--- py/make_data_file_list.py
+++ py/make_data_file_list.py
@@ -1,4 +1,3 @@
-import ROOT
 import os
 import re
 import sys
@@ -8,7 +7,6 @@ import getopt
 from subprocess import call
 
 # global parameters
-temp_dir = '../'
 
 def get_py_dir():
     """Returns full path to this file"""
@@ -17,9 +15,47 @@ def get_py_dir():
 def get_proj_dir():
     """Returns full path to project.
     It is assumed that this file was placed in project_dir/py directory"""
-    return os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    return os.path.dirname(get_py_dir())
 
+def get_tmp_dir():
+    """Returns full path to directory with temporal files
+    project_dir/tmp
+    Creates it if it does not exists"""
+    tmp = '%s/tmp' % get_proj_dir()
+    
+    if not os.path.exists(tmp):
+        os.makedirs(tmp)
+    return tmp
+
+def save_file_list(data_dir=None, out_filename="data_files.txt", patt="C\d+_FBLM_\d+_00000\.txt"):
+    """Looks for files in data_dir and its subfolders and
+    writes all filenames that matches to patt in tmp_dir/out_filename text file"""
+    pat = re.compile(patt)
+    if not data_dir:
+        data_dir = os.getcwd()
+    
+    data_files_list = [];
+    for path, subdirs, files in os.walk(data_dir):
+        data_files_list.extend([os.path.join(path, name) for name in files if pat.match(name)]) 
+            
+    with open(os.path.join(get_tmp_dir(), "data_files.txt"), 'w') as tmp:
+        tmp.write('\n'.join(data_files_list))
+ 
+       
 if __name__ == "__main__":
-    print get_py_dir()
+    pat = re.compile("C\d+_FBLM_\d+_00000\.txt")
     
-    
+    data_dir = None
+    opts, args = getopt.getopt(sys.argv[1:], 'd:', [])
+    for o, a in opts:
+        if o == '-d':
+            if not os.path.exists(a) or not os.path.isdir(a):
+                print "Wrong path to data specified"
+                print "The full path must be specified as an argument"
+            else:
+                data_dir = a
+                
+    save_file_list(data_dir=data_dir)
+
+
+
